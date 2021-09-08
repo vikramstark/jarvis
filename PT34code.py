dfb757189a13393d64cb0532b1e8b71dc0f59755
vikramstark <vikram.rakr@gmail.com>
2021-09-08 21:24:37 +0530

    one more file added

---
+++ PT34code.py
@@ -0,0 +1,19 @@
+# # Eployee --> salary,
+
+# Employee
+# Student
+
+# Just an analogy
+# noun --> classes 
+# adjective --> atributes
+# verb --> methods
+
+
+class Employee:
+    salary = 34
+    name = "Vikram"
+    def getSalary(self):
+            return self.salary
+e1 = Employee()
+e1.salary = 766
+print(e1.salary)
